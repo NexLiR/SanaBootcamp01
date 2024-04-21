c2ea9c63af4c68309a66cdf7bf6ef4620d13a17f
Shevchuk Vladyslav <ipz231_shvye@student.ztu.edu.ua>
2024-04-21 20:21:58 +0300

    Add project files.

---
+++ Project/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project.Models;
+using System.Diagnostics;
+
+namespace Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Welcome()
+        {
+            string name = TempData["UserName"] as string;
+            return View("Welcome", name);
+        }
+
+        [HttpPost]
+        public IActionResult Check(WelcomeToMySite user)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["UserName"] = user.Name;
+                return RedirectToAction("Welcome");
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Project/Models/WelcomeToMySite.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project.Models
+{
+    public class WelcomeToMySite
+    {
+        [Display(Name = "Enter your name:")]
+        [Required(ErrorMessage = "Please enter your name.")]
+        public string Name { get; set; }
+
+        public WelcomeToMySite()
+        {
+            Name = "Guest";
+        }
+        public WelcomeToMySite(string name)
+        {
+            Name = name;
+        }
+    }
+}
